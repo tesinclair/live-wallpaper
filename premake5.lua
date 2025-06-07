3b0c113ecf723800e347cef583238243e85d9c4e
Tai Sinclair <taisinclair4@gmail.com>
2025-06-07 15:56:22 +0100

    adds main test file and adds testing options to premake rebases master

--- premake5.lua
+++ premake5.lua
@@ -1,21 +1,50 @@
-
 workspace "live-wallpaper"
     configurations { "Debug", "Release" }
 
-project "live-wallpaper"
+    newoption {
+        trigger = "test",
+        description = "Build tests"
+    }
+
+if not _OPTIONS["test"] then
+    project "live-wallpaper"
+        kind "ConsoleApp"
+        language "C++"
+        targetdir "bin/%{cfg.buidcfg}"
+
+        files { 
+            "src/main.cpp",
+            -- libs --
+            "src/lib/reader.cpp",
+            "src/lib/reader.h",
+            "src/lib/customErr.h"
+        }
+
+        filter "configurations:Debug"
+            defines { "DEBUG" }
+            symbols "On"
+
+        filter "configurations:Release"
+            defines { "NDEBUG" }
+            optimize "On"
+end
+
+if _OPTIONS["test"] then
+    project "test-live-wallpaper"
     kind "ConsoleApp"
     language "C++"
-    targetdir "bin/%{cfg.buidcfg}"
+    targetdir "bin/%{cfg.buildcfg}/tests"
 
-    files { "src/main.cpp", "src/lib/reader.cpp", "src/lib/reader.h", "src/lib/customErr.h" }
+    files { 
+        -- test files --
+        "tests/**.test.cpp",
+        "tests/**.test.h",
+        -- lib files for testing -- 
+        "src/lib/reader.cpp",
+        "src/lib/reader.h",
+        "src/lib/customErr.h" 
+    }
 
-    filter "configurations:Debug"
-        defines { "DEBUG" }
-        symbols "On"
-
-    filter "configurations:Release"
-        defines { "NDEBUG" }
-        optimize "On"
-
-
-
+    defines { "DEBUG" }
+    symbols "On"
+end
