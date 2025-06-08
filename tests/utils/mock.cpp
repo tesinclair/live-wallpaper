1760ffb620204f6a5c421ab23cb1362698e1d773
Tai Sinclair <taisinclair4@gmail.com>
2025-06-08 16:55:32 +0100

    adds mp4 mocking skeletons

---
+++ tests/utils/mock.cpp
@@ -0,0 +1,9 @@
+#include "mock.h"
+
+int mockMP4(char* filename){
+    return 1;
+}
+
+int removeMP4Mock(char* filename){
+    return 1;
+}
---
+++ tests/utils/mock.h
@@ -0,0 +1,3 @@
+int mockMP4(char* filename);
+
+int removeMP4Mock(char* filename);
