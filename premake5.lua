be312c961501ce3c3d273b1fba3d8403521a46f8
Tai Sinclair <taisinclair4@gmail.com>
2025-06-22 21:59:59 +0100

    adds logging build options to premake

--- premake5.lua
+++ premake5.lua
@@ -1,25 +1,43 @@
-
 workspace "live-wallpaper"
     configurations { "Debug", "Release" }
+
+    newoption {
+        trigger = "verbose",
+        description = "Build with all logging in release mode"
+    }
+
+    newoption {
+        trigger = "silent",
+        description = "Build will no logging in debug mode"
+    }
 
 project "live-wallpaper"
     kind "ConsoleApp"
     language "C++"
-    targetdir "bin/%{cfg.buidcfg}"
+    targetdir "bin/%{cfg.buildcfg}"
 
     includedirs {
         "third_party/*"
     }
 
-    files { "src/**"" }
+    files { "src/**" }
 
     filter "configurations:Debug"
-        defines { "DEBUG" }
+        defines { 
+            "DEBUG",
+            'ROOT="' .. _MAIN_SCRIPT_DIR .. '"'
+        }
         symbols "On"
 
-    filter "configurations:Release"
+    filter { "configurations:Debug", "options:silent" }
+        defines { "SILENT" }
+
+    filter "configurations:Release" 
         defines { "NDEBUG" }
         optimize "On"
+
+    filter { "configurations:Release", "options:verbose"}
+        defines { "VERBOSE" }
 
 
 
