7730abfadf9f1e5a9d5927de2a0cb2e7cc9d66fb
Tai Sinclair <taisinclair4@gmail.com>
2025-11-09 00:55:23 +0000

    adds decoder

--- go.mod
+++ go.mod
@@ -4,4 +4,8 @@ go 1.24.2
 
 require (
 	github.com/akamensky/argparse v1.4.0 // indirect
+	github.com/aws/aws-sdk-go v1.55.8 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/u2takey/ffmpeg-go v0.5.0 // indirect
+	github.com/u2takey/go-utils v0.3.1 // indirect
 )
--- services/decoder.go
+++ services/decoder.go
@@ -1,12 +1,45 @@
 package services 
 
+import (
+	ffmpeg "github.com/u2takey/ffmpeg-go" 
+)
+
 type Decoder struct{
 	file string
 	server string
 	screenHeight uint
 	screenWidth uint
+	// TODO(tesinclair): add filter options more than just crop
 }
 
+/*
+* @param fname: must be the absolute path to the input file
+*/
 func CreateDecoder(fname string, addr string) *Decoder{
+	return &Decoder{
+		file: fname,
+		server: addr
+	}
+}
+
+func (d *Decoder) SetScreenDimensions(w uint8, h uint8){
+	d.screenHeight = h
+	d.screenWidth = w
+}
+
+func (d *Decoder) Decode(){
+	err := ffmpeg.Input(d.file, ffmpeg.KwArgs{"re":""}).
+			Filter("crop", ffmpeg.Args{
+				fmt.Sprintf("%v:%v", d.screenWidth, d.screenHeight)
+			}).
+			Output(d.server, ffmpeg.KwArgs{
+				"c:v": "libx264",
+				"b:v": "2000k",
+				"f": "mpegts",
+			}).
+			Run()
+	if err != nil{
+		panic(err)
+	}
 }
 
